0e31d94f724a64be3d5979e2fb17070ff88fc434
ddhuu <ddhuu.dev@gmail.com>
2024-01-21 15:46:47 +0700

    setup project

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const compression = require('compression')
+const express = require ('express')
+const { default: helmet } = require('helmet')
+const morgan = require('morgan')
+const app = express()
+
+
+
+// init middlewares
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(compression())
+
+
+
+// init db
+
+
+
+// Init Routeme
+app.get('/', (req,res,next)=>{
+    const strCompress = "Hi"
+    return res.status(200).json({
+        message: 'Hello World',
+        metadata: strCompress.repeat(100000)
+
+    })
+})
+
+// Handling Error
+
+
+
+
+
+module.exports = app 
