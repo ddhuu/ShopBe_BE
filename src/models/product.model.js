dd6b1187dc896f020e6d728c8be3220e7032e373
ddhuu <ddhuu.dev@gmail.com>
2024-04-02 14:10:25 +0700

    Product Schema

---
+++ src/models/product.model.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const { model, Schema } = require("mongoose");
+
+const DOCUMENT_NAME = "Product";
+const COLLECTION_NAME = "Products";
+
+const productSchema = new Schema(
+  {
+    product_name: { type: String, required: true },
+    product_thumb: { type: String, required: true },
+    product_description: String,
+    product_price: { type: Number, required: true },
+    product_quantity: { type: Number, required: true },
+    product_type: {
+      type: String,
+      required: true,
+      enum: ["Electronics", "Clothing", "Furniture"],
+    },
+    product_shop: {type: Schema.Types.ObjectId, ref:'Shop'},
+    product_attributes: { type: Schema.Types.Mixed, required: true },
+  },
+  {
+    collection: COLLECTION_NAME,
+    timestamps: true,
+  }
+);
+
+// define the product type => Clothing
+
+const clothingSchema = new Schema(
+  {
+    brand: { type: String, required: true },
+    size: String,
+    material: String,
+  },
+  {
+    collection: "clothes",
+    timestamps: true,
+  }
+);
+
+// define the product type => Electronic
+
+const electronicSchema = new Schema(
+  {
+    manufacturor: { type: String, required: true },
+    model: String,
+    color: String,
+  },
+  {
+    collection: "electronics",
+    timestamps: true,
+  }
+);
+
+
+module.exports = {
+    product: model(DOCUMENT_NAME, productSchema),
+    clothing: model('Clothing', electronicSchema),
+    product: model('Electronics', clothingSchema),
+}
